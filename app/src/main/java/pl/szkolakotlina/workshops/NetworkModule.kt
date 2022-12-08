a5b63c9f140fb4a10945b053bc58fefb7185a4d1
jaroslawmichalik <michalik@protonmail.ch>
2022-12-08 19:39:39 +0100

    Workshops 8.12.2022

--- app/src/main/java/pl/szkolakotlina/workshops/NetworkModule.kt
+++ app/src/main/java/pl/szkolakotlina/workshops/NetworkModule.kt
@@ -10,11 +10,12 @@ object NetworkModule {
     fun createApi(): ArticlesApi = Retrofit.Builder()
         .addConverterFactory(RssConverterFactory.create())
         .client(OkHttpClient())
-        .baseUrl("https://kotlintesting.com")
+        .baseUrl("https://kotlintesting.com/")
         .build()
         .create(ArticlesApi::class.java)
 }
 
+//https://picsum.photos/500
 interface ArticlesApi {
     @GET("/rss")
     suspend fun getRssFeed(): RssFeed
